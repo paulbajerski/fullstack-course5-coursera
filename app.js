1b598bb1f15fdfc6844f59e124cafdd387b0fab9
Paul Bajerski <paul.bajerski@davispolk.com>
2021-04-15 16:39:09 -0400

    protecting dependency injection from minification

--- app.js
+++ app.js
@@ -2,23 +2,19 @@
 	'use strict';
 
 	angular
-		.module('NameCalculator', [])
-		.controller('NameCalculatorController', function ($scope) {
-			$scope.name = '';
-			$scope.totalValue = 0;
-			$scope.displayNumeric = function (string) {
-				var totalNameValue = calculateNumericForString($scope.name);
-				$scope.totalValue = totalNameValue;
-			};
+		.module('DIApp', [])
+		.controller('DIController', DIController);
 
-			function calculateNumericForString(string) {
-				var totalStringValue = 0;
-				for (var i = 0; i < string.length; i++) {
-					totalStringValue += string.charCodeAt(i);
-				}
-				return totalStringValue;
-			}
-		});
+	DIController.$inject = ['$scope', '$filter'];
+
+	function DIController($scope, $filter) {
+		$scope.name = 'Pawel';
+
+		$scope.upper = function () {
+			var upCase = $filter('uppercase');
+			$scope.name = upCase($scope.name);
+		}
+	}
 
 
 })();
