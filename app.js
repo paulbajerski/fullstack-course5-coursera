e441962a4bd9b4e6d4dda5732862812c5ec8ad6e
Paul Bajerski <paul.bajerski@davispolk.com>
2021-04-19 16:25:29 -0400

    creating custom filters with additional arguments and chaining

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 	angular.module('MsgApp', [])
 		.controller('MsgController', MsgController)
 		.filter('loves', LovesFilter)
+		.filter('truth', TruthFilter)
 
 	MsgController.$inject = ['$scope', 'lovesFilter'];
 	function MsgController($scope, lovesFilter) {
@@ -29,6 +30,14 @@
 		return function (input) {
 			input = input || "";
 			input = input.replace("likes", "loves");
+			return input;
+		}
+	}
+
+	function TruthFilter() {
+		return function (input, target, replace) {
+			input = input || "";
+			input = input.replace(target, replace);
 			return input;
 		}
 	}
