8d84f6ac74b386e8524204a336de6cfdffdf378e
Paul Bajerski <paul.bajerski@davispolk.com>
2021-04-20 16:06:52 -0400

    ng-repeat filter

--- app.js
+++ app.js
@@ -1,27 +1,8 @@
 (function () {
 	'use strict';
 
-	var shoppingList1 = [
+	var shoppingList = [
 		"Milk", "Donuts", "Cookies", "Chocolate", "Peanut Butter", "Pepto Bismol", "Pepto Bismol (Chocolate flavor)", "Pepto Bismol (Cookie flavor)"
-	];
-
-	var shoppingList2 = [
-		{
-			name: "Milk",
-			quantity: "2"
-		},
-		{
-			name: "Donuts",
-			quantity: "200"
-		},
-		{
-			name: "Cookies",
-			quantity: "300"
-		},
-		{
-			name: "Chocolate",
-			quantity: "5"
-		}
 	];
 
 	angular.module('ShoppingListApp', [])
@@ -29,30 +10,7 @@
 
 	ShoppingListController.$inject = ['$scope'];
 	function ShoppingListController($scope) {
-		$scope.shoppingList1 = shoppingList1;
-		$scope.shoppingList2 = shoppingList2;
-
-		$scope.addToList = function () {
-			var newItem = {
-				name: $scope.newItemName,
-				quantity: $scope.newItemQuantity
-			}
-			$scope.shoppingList2.push(newItem);
-		};
+		$scope.shoppingList = shoppingList;
 	}
 
 })();
-
-/*
-
-Summary
-
-ng-repeat is a direcive that extends the functionality of HTML elements it's applied to
-	- behaves very similar to for-each construct
-
-ng-repeat="item in collection", where item can now be used in interpolation as an item in the collection at particular index of iteration
-
-ng-repeat exposes a special $index property to the body of its host tag
-	- holds the numeric index of the curren titem in the looop
-
-*/
