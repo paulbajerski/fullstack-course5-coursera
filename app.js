a8c25e51b26452944d01dcaab80a014473bde654
Paul Bajerski <paul.bajerski@davispolk.com>
2021-04-26 13:29:15 -0400

    .factory()

--- app.js
+++ app.js
@@ -2,46 +2,76 @@
   "use strict";
 
   angular
-    .module("ShoppingListApp", [])
-    .controller("ShoppingListAddController", ShoppingListAddController)
-    .controller("ShoppingListShowController", ShoppingListShowController)
-    .service("ShoppingListService", ShoppingListService);
+    .module("ControllerAsApp", [])
+    .controller("ShoppingListController1", ShoppingListController1)
+    .controller("ShoppingListController2", ShoppingListController2)
+    .factory("ShoppingListFactory", ShoppingListFactory);
 
-  ShoppingListAddController.$inject = ["ShoppingListService"];
-  function ShoppingListAddController(ShoppingListService) {
-    var itemAdder = this;
+  // LIST #1 - controller
+  ShoppingListController1.$inject = ["ShoppingListFactory"];
+  function ShoppingListController1(ShoppingListFactory) {
+    var list1 = this;
 
-    itemAdder.itemName = "";
-    itemAdder.itemQuantity = "";
+    // Use factory to create new shopping list service
+    var shoppingList = ShoppingListFactory();
 
-    itemAdder.addItem = function () {
-      ShoppingListService.addItem(itemAdder.itemName, itemAdder.itemQuantity);
+    list1.items = shoppingList.getItems();
+
+    list1.itemName = "";
+    list1.itemQuantity = "";
+
+    list1.addItem = function () {
+      shoppingList.addItem(list1.itemName, list1.itemQuantity);
+    };
+
+    list1.removeItem = function (itemIndex) {
+      shoppingList.removeItem(itemIndex);
     };
   }
 
-  ShoppingListShowController.$inject = ["ShoppingListService"];
-  function ShoppingListShowController(ShoppingListService) {
-    var showList = this;
+  // LIST #2 - controller
+  ShoppingListController2.$inject = ["ShoppingListFactory"];
+  function ShoppingListController2(ShoppingListFactory) {
+    var list2 = this;
 
-    showList.items = ShoppingListService.getItems();
+    // Use factory to create new shopping list service
+    var shoppingList = ShoppingListFactory(3);
 
-    showList.removeItem = function (itemIndex) {
-      ShoppingListService.removeItem(itemIndex);
+    list2.items = shoppingList.getItems();
+
+    list2.itemName = "";
+    list2.itemQuantity = "";
+
+    list2.addItem = function () {
+      try {
+        shoppingList.addItem(list2.itemName, list2.itemQuantity);
+      } catch (error) {
+        list2.errorMessage = error.message;
+      }
+    };
+
+    list2.removeItem = function (itemIndex) {
+      shoppingList.removeItem(itemIndex);
     };
   }
 
-  function ShoppingListService() {
+  // If not specified, maxItems assumed unlimited
+  function ShoppingListService(maxItems) {
     var service = this;
 
     // List of shopping items
     var items = [];
 
     service.addItem = function (itemName, quantity) {
-      var item = {
-        name: itemName,
-        quantity: quantity
-      };
-      items.push(item);
+      if (maxItems === undefined || (maxItems !== undefined && items.length < maxItems)) {
+        var item = {
+          name: itemName,
+          quantity: quantity
+        };
+        items.push(item);
+      } else {
+        throw new Error("Max items (" + maxItems + ") reached.");
+      }
     };
 
     service.removeItem = function (itemIndex) {
@@ -52,19 +82,27 @@
       return items;
     };
   }
+
+  function ShoppingListFactory() {
+    var factory = function (maxItems) {
+      return new ShoppingListService(maxItems);
+    };
+
+    return factory;
+  }
 })();
 
 /*
 SUMMARY
 
-Controllers are not suppose to
-	- handle business logic
-	- code sharing
-	- be used to share data across other components
+.factory() allows us to produce any type of object or function
+  - that includes a service (even a singleton), but is NOT limited to
+  - .service() is just a more limited factory
 
-Custom services instantiated with .service method
-	- singletons (only 1 instance of object exists)
-	- lazily instantiated (only created if someting depends on them)
+.factory('name', FactoryFunction) - name is what's injected
 
-.service('name', function), treats function as a function constructor
+Injected factory function refers to whatever is returned in the factory function
+  - can be object literal with a prop that's a function that creates something
+  - can be a function that creates something
+
 */
